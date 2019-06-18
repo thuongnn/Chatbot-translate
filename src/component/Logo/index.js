76519f2b6e69a58f057435b3570eb401dd83d569
jmthuong97 <jmthuong97@gmail.com>
2019-06-18 11:18:24 +0700

    first push

---
+++ src/component/Logo/index.js
@@ -0,0 +1,10 @@
+import './index.css';
+import React from "react";
+
+export default () => {
+    return (
+        <h1 className="logo">
+            <span className="word">Doublet</span>
+        </h1>
+    );
+};
